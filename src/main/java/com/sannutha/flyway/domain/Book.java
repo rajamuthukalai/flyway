d8192fc15bb911c8000040dd41b016ef918931a7
Raja <rajanm.apps@gmail.com>
2024-06-01 20:32:54 +1000

    flyway tutorial

---
+++ src/main/java/com/sannutha/flyway/domain/Book.java
@@ -0,0 +1,21 @@
+package com.sannutha.flyway.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "book")
+public class Book {
+    @Id
+    private String id;
+    private String title;
+}
---
+++ src/main/resources/db/migration/V1__initial_schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE "book" (
+    "id" text NOT NULL,
+    "title" text,
+    CONSTRAINT "book.pkey" PRIMARY KEY("id")
+);
